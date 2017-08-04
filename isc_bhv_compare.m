e27cc9eb8b2bc873a6e289a624c5e877b8646b29
hcchang73 <b91a01301@ntu.edu.tw>
2017-08-04 09:58:52 -0400

    just to replicate stephens

---
+++ isc_bhv_compare.m
@@ -0,0 +1,48 @@
+
+clear all
+close all
+set_parameters
+thr=0.18;
+
+intervals=[-20 -2;
+    -1 1;
+    2 20
+    ]
+interval_names={'l_precede','sl_syn','s_precede'}
+for ei=1%:2;
+    exp= experiments{ei};
+    
+    load([expdir exp '/fmri/mat/wholeBrain/' exp '_isc_listener_peakR.mat' ],'isc_peakR');
+    load([expdir exp '/fmri/mat/wholeBrain/' exp '_isc_listener_peakT.mat' ],'isc_peakT');
+    
+    r=zeros(size(isc_peakR));
+    voxn=[];
+    
+    for intvi = 1:size(intervals,1);
+        for si=1:size(isc_peakR,3);
+            i=find(isc_peakR(:,:,si)>thr & isc_peakT(:,:,si) >= intervals(intvi,1) & isc_peakT(:,:,si) <= intervals(intvi,2));
+            r(i,:,si)=isc_peakR(i,:,si);
+            voxn(si,intvi)=sum(r(:,:,si)~=0);
+        end
+        
+%         nii.hdr=load_nii_hdr([expdir 'roi_mask/MNI152_T1_3mm_brain.nii']);
+%         data=mean(r,3);
+%         data((end+1):(61*73*61),:)=0;
+%         nii.img=reshape(data,61,73,61);
+%         save_nii(nii,[expdir exp '/fmri/nii/wholeBrain/' exp '_' interval_names{intvi} '.nii']);
+    clear r
+    end
+end
+
+load([expdir exp '\bhv_merlin2.mat'])
+figure;
+ for intvi = 1:size(intervals,1);
+[seq_new1, rk1]  = sort(bhv.scores);
+[seq_new2, rk2]  = sort(voxn(:,intvi));
+subplot(1,3,intvi);
+
+scatter(rk1,rk2);
+title(interval_names{intvi});
+ end
+ 
+
